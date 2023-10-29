7534c7076f7a0a94dad96c4e3d1747a067a7085b
Mahmudul Hasan <dev.mahmudulhasan2@gmail.com>
2023-10-29 12:58:04 +0600

    set the next and prevew button

--- script.js
+++ script.js
@@ -7,37 +7,63 @@ const apiURL = "https://api.lyrics.ovh";
 
 // Search by song or artist
 async function searchSongs(term) {
-  const res = await fetch(`${apiURL}/suggest/(${term})`);
+  const res = await fetch(`${apiURL}/suggest/${term}`);
   const data = await res.json();
   showData(data);
 }
 
-// Show song and artist in DOM
+// Show song and artist in the DOM.
 function showData(data) {
   let output = "";
 
-  data.data.foEach((song) => {
+  data.data.forEach((song) => {
     output += `
     <li>
-    <span><strong>${song.artist.name}</strong> - $</span>
-    <button class="btn" data-articst="{song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+      <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+      <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
     </li>
     `;
   });
+
   result.innerHTML = `
-  <ul class="song">
-  ${output}
+  <ul class="songs">
+    ${output}
   </ul>
   `;
+
+  if (data.prev || data.next) {
+    more.innerHTML = `
+      ${
+        data.prev
+          ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>`
+          : ""
+      }
+      ${
+        data.next
+          ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>`
+          : ""
+      }
+    `;
+  } else {
+    more.innerHTML = "";
+  }
 }
 
-// Event listners
+// Get previous and next songs
+async function getMoreSongs(url) {
+  const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+  const data = await res.json();
+
+  showData(data);
+}
+
+// Event listener for the form submission
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const searchTerm = search.value.trim();
   if (!searchTerm) {
-    alert("please type a search term");
+    alert("Please type a search term");
   } else {
     searchSongs(searchTerm);
   }
