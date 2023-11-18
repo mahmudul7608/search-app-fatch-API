c3708732416650ff8c4416c0c40c8a571acabe3e
Mahmudul Hasan <dev.mahmudulhasan2@gmail.com>
2023-11-19 00:00:46 +0600

    add js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const form = document.getElementById("form");
 const search = document.getElementById("search");
-const result = document.getElementById("result"); // Corrected ID
+const result = document.getElementById("result");
 const more = document.getElementById("more");
 
 const apiURL = "https://api.lyrics.ovh";
@@ -16,22 +16,46 @@ async function searchSongs(term) {
 
 // Show song and artist in DOM
 function showData(data) {
-  let output = "";
-
-  data.data.forEach((song) => {
-    output += `
-    <li>
-      <span><strong>${song.artist.name}</strong> - ${song.title}</span> <!-- Corrected property name -->
-      <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button> <!-- Corrected property name -->
-    </li>
-    `;
-  });
+  const output = data.data
+    .map(
+      (song) => `
+      <li>
+        <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+        <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+      </li>`
+    )
+    .join(""); // Move join outside of the template literal
 
   result.innerHTML = `
-   <ul class="songs">
-   ${output}
-   </ul>
-   `;
+    <ul class="songs">
+      ${output}
+    </ul>
+  `;
+
+  if (data.prev || data.next) {
+    more.innerHTML = `
+    ${
+      data.prev
+        ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>`
+        : ""
+    }
+    ${
+      data.next
+        ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>`
+        : ""
+    }
+    `;
+  } else {
+    more.innerHTML = "";
+  }
+}
+
+// Get prev and next songs
+async function getMoreSongs(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  showData(data);
 }
 
 // Event listener for the form submission
@@ -45,27 +69,3 @@ form.addEventListener("submit", (e) => {
     searchSongs(searchTerm);
   }
 });
-
-// Event listener for the form submission
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const searchTerm = search.value.trim();
-  if (!searchTerm) {
-    alert("Please type in a search term");
-  } else {
-    searchSongs(searchTerm);
-  }
-});
-
-// // Get lyrics button click
-// result.addEventListener("click", (e) => {
-//   const clickedEl = e.target;
-
-//   if (clickedEl.tagName === "BUTTON") {
-//     const artist = clickedEl.getAttribute("data-artist");
-//     const songTitel = clickedEl.getAttribute("data-songtitle");
-
-//     getLyrics(artist, songTitel);
-//   }
-// });
